fa4455f3f8de701477b6c839bfc9b69fab053594
Javier <javierinoa2@gmail.com>
2024-06-14 15:01:33 -0400

    Javier
    APPver0.1

---
+++ APPpanafie/src/main/java/panafie/fie/PanaFieApplication.java
@@ -0,0 +1,13 @@
+package panafie.fie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanaFieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanaFieApplication.class, args);
+	}
+
+}
---
+++ APPpanafie/src/test/java/panafie/fie/PanaFieApplicationTests.java
@@ -0,0 +1,13 @@
+package panafie.fie;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanaFieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
