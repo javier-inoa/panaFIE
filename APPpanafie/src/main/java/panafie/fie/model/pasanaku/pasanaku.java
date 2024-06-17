69048495f15c521b3d9349949dac84bff18ad905
Javier <javierinoa2@gmail.com>
2024-06-17 17:37:38 -0400

    Data NoArgs AllArgs
    pasanaku, moderator

--- APPpanafie/src/main/java/panafie/fie/model/pasanaku/pasanaku.java
+++ APPpanafie/src/main/java/panafie/fie/model/pasanaku/pasanaku.java
@@ -6,14 +6,20 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "pasanaku")
-public class pasanaku {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pasanaku {
 
     @Id 
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    private Long id;
     
     private String nombre;
     private int monto;
@@ -23,79 +29,6 @@ public class pasanaku {
     private LocalDate duracion;
     private String moderador;
 
-
-    public pasanaku() {
-    }
-
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public int getMonto() {
-        return monto;
-    }
-
-    public void setMonto(int monto) {
-        this.monto = monto;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public String getReglas() {
-        return reglas;
-    }
-
-    public void setReglas(String reglas) {
-        this.reglas = reglas;
-    }
-
-    public LocalDate getFechaInicio() {
-        return fechaInicio;
-    }
-
-    public void setFechaInicio(LocalDate fechaInicio) {
-        this.fechaInicio = fechaInicio;
-    }
-
-    public LocalDate getDuracion() {
-        return duracion;
-    }
-
-    public void setDuracion(LocalDate duracion) {
-        this.duracion = duracion;
-    }
-
-    public String getModerador() {
-        return moderador;
-    }
-
-    public void setModerador(String moderador) {
-        this.moderador = moderador;
-    }
-
-
-    public void sortearBeneficiarios() {
-
-    }
 }
 
 
