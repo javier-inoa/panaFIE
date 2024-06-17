e95148ef712bd1280594f6887bbf958aa4802d87
itzel <nayeliticona@gmail.com>
2024-06-17 17:26:41 -0400

    model pasanaku and moderator

--- APPpanafie/src/main/java/panafie/fie/model/moderator/moderator.java
+++ APPpanafie/src/main/java/panafie/fie/model/moderator/moderator.java
@@ -1,5 +1,74 @@
 package panafie.fie.model.moderator;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "moderator")
 public class moderator {
 
+    @Id 
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    
+    private String rol = "moderador";
+    private int cantidadJuegos;
+
+
+    public moderator() {
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public int getCantidadJuegos() {
+        return cantidadJuegos;
+    }
+
+    public void setCantidadJuegos(int cantidadJuegos) {
+        this.cantidadJuegos = cantidadJuegos;
+    }
+
+
+    public void login() {
+
+    }
+
+    public void sortearPasanaku() {
+
+    }
+
+    public void visualizarPasanaku() {
+
+    }
+
+    public void enviarComentarioJugadores() {
+
+    }
+
+    public void notificarAdministrador() {
+
+    }
+
+    public void controlPagos() {
+
+    }
 }
--- APPpanafie/src/main/java/panafie/fie/model/pasanaku/pasanaku.java
+++ APPpanafie/src/main/java/panafie/fie/model/pasanaku/pasanaku.java
@@ -1,5 +1,125 @@
 package panafie.fie.model.pasanaku;
 
+import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pasanaku")
 public class pasanaku {
 
+    @Id 
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    
+    private String nombre;
+    private int monto;
+    private String descripcion;
+    private String reglas;
+    private LocalDate fechaInicio;
+    private LocalDate duracion;
+    private String moderador;
+
+
+    public pasanaku() {
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getMonto() {
+        return monto;
+    }
+
+    public void setMonto(int monto) {
+        this.monto = monto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getReglas() {
+        return reglas;
+    }
+
+    public void setReglas(String reglas) {
+        this.reglas = reglas;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public LocalDate getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(LocalDate duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getModerador() {
+        return moderador;
+    }
+
+    public void setModerador(String moderador) {
+        this.moderador = moderador;
+    }
+
+
+    public void sortearBeneficiarios() {
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
