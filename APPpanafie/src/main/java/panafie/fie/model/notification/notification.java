c14672a3bfd3b73be378e1421bee22d7f3d64256
Alejandro <aljaregu@gmail.com>
2024-06-17 16:34:24 -0400

    [change] model name changed

--- APPpanafie/src/main/java/panafie/fie/model/notification/notification.java
+++ APPpanafie/src/main/java/panafie/fie/model/notification/notification.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 
 @Data @NoArgsConstructor @AllArgsConstructor @Entity
 @Table(name = "notification")
-public class notification {
+public class Notification {
     
     @Id @GeneratedValue
     private Long id;
--- APPpanafie/src/main/java/panafie/fie/model/person/person.java
+++ APPpanafie/src/main/java/panafie/fie/model/person/person.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 
 @Data @NoArgsConstructor @AllArgsConstructor @Entity
 @Table(name = "person")
-public class person {
+public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
