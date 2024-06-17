1433a2fd21338935ba6ca6fba004236b62cc7fb6
Alejandro <aljaregu@gmail.com>
2024-06-17 16:01:42 -0400

    [add] Model Notification and Person

--- APPpanafie/src/main/java/panafie/fie/model/notification/notification.java
+++ APPpanafie/src/main/java/panafie/fie/model/notification/notification.java
@@ -1,5 +1,27 @@
 package panafie.fie.model.notification;
 
-public class notification {
+import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data @NoArgsConstructor @AllArgsConstructor @Entity
+@Table(name = "notification")
+public class notification {
+    
+    @Id @GeneratedValue
+    private Long id;
+    
+    private String tipo;
+    
+    private String mensaje;
+    
+    private LocalDate fecha;
+    
+    private String destinatario;
+    
 }
